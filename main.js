d54122294fc48ef1aad8c91cdddc745c38cade93
--replace-all <ptmceldowney@gmail.com>
2024-05-19 15:35:08 -0500

    adding vite to project

---
+++ main.js
@@ -0,0 +1,20 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  
+`;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.getElementById('visualizer');
+  const ctx = canvas.getContext('2d');
+
+  let audioSource;
+  let anaylser;
+});
+
+async function setupVisuazlier(accessToken) {
+  try {
+  } catch (e) {
+    console.error('Error in the visualizer');
+  }
+}
