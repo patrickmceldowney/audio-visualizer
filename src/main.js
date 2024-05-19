3ed11b6197adddc18183566290fad7144dfbacd4
--replace-all <ptmceldowney@gmail.com>
2024-05-19 17:31:43 -0500

    using spotify web sdk

--- src/main.js
+++ src/main.js
@@ -10,21 +10,67 @@ document.getElementById('login-button').addEventListener('click', () => {
   window.location.href = authUrl;
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const hash = window.location.substring(1);
+window.addEventListener('load', () => {
+  const hash = window.location.hash.substring(1);
   const params = new URLSearchParams(hash);
-  const accessToken = params.get(access_token);
+  const accessToken = params.get('access_token');
 
-  console.log('access token', accessToken);
   if (accessToken) {
     document.getElementById('login-button').style.display = 'none';
+    document.getElementById('toggle-play').style.display = 'block';
+
     document.getElementById('visualizer-container').style.display = 'block';
-    setupVisualizer(accessToken);
+    if (window.Spotify) {
+      setupSpotifyPlayer(accessToken);
+    }
   }
 });
 
-async function setupVisualizer(accessToken) {
-  try {
+function setupSpotifyPlayer(accessToken) {
+  window.onSpotifyWebPlaybackSDKReady = () => {
+    const player = new Spotify.Player({
+      name: 'Spotify Audio Visualizer',
+      getOAuthToken: (cb) => {
+        cb(accessToken);
+      },
+      volume: 0.5,
+    });
+
+    player.addListener('initialization_error', ({ message }) => {
+      console.error(message);
+    });
+    player.addListener('authentication_error', ({ message }) => {
+      console.error(message);
+    });
+    player.addListener('account_error', ({ message }) => {
+      console.error(message);
+    });
+    player.addListener('playback_error', ({ message }) => {
+      console.error(message);
+    });
+    player.addListener('player_state_changed', (state) => {
+      if (state) {
+        visualize(state);
+      }
+    });
+
+    player.addListener('ready', ({ device_id }) => {
+      console.log('Ready with Device ID', device_id);
+    });
+
+    player.addListener('not_ready', ({ device_id }) => {
+      console.log('Device ID has gone offline', device_id);
+    });
+
+    document.getElementById('toggle-play').onclick = function () {
+      player.togglePlay();
+      console.log('here');
+    };
+
+    player.connect();
+  };
+
+  function visualize(state) {
     const canvas = document.getElementById('visualizer');
     const ctx = canvas.getContext('2d');
     canvas.width = canvas.clientWidth;
@@ -36,20 +82,18 @@ async function setupVisualizer(accessToken) {
 
     const bufferLength = anaylser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    const source = audioCtx.createMediaElementSource(stream);
+    const track = state.track_window.current_track;
+    console.log(
+      `Now playing: ${track.name} by ${track.artists
+        .map((artist) => artist.name)
+        .join(', ')}`
+    );
+    const source = audioCtx.createMediaElementSource(new Audio(track.uri));
     source.connect(anaylser);
+    anaylser.connect(audioCtx.destination);
 
-    async function getPlaybackState() {
-      const res = await fetch('https://api.spotify.com/v1/me/player', {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      return res.json();
-    }
-
-    function visualize() {
+    function draw() {
+      requestAnimationFrame(draw);
       anaylser.getByteFrequencyData(dataArray);
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -70,24 +114,8 @@ async function setupVisualizer(accessToken) {
         ctx.fillRect(x, canvas.height - barHeight / 2, barWidth, barHeight / 2);
         x += barWidth + 1;
       }
-
-      requestAnimationFrame(visualize);
     }
 
-    visualize();
-
-    setInterval(async () => {
-      const playback = await getPlaybackState();
-      if (playback && playback.is_playing) {
-        const track = playback.item;
-        console.log(
-          `Now play: ${track.name} by ${track.artists
-            .map((artist) => artist.name)
-            .join(', ')}`
-        );
-      }
-    }, 1000);
-  } catch (e) {
-    console.error('Error in the visualizer');
+    draw();
   }
 }
